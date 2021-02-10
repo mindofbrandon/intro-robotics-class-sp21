336124e17d5b5460753c7c37510545f6a766ca8a
brandon <brandon_zuniga@student.uml.edugit config --global user.email brandon_zuniga@student.uml.edugit config --global user.email brandon_zuniga@student.uml.edu>
2021-02-10 18:31:20 -0500

    updated node1.py to fix error

--- packages/package1/src/node1.py
+++ packages/package1/src/node1.py
@@ -10,21 +10,24 @@ class Talker:
         self.pub = rospy.Publisher('chatter', String, queue_size=10)
 
     def talk(self):
-        n1, n2 = 0, 1           # initial fib terms
+        n1, n2 = 0, 1
+        while True:
 
-        rospy.loginfo(n1)
-        self.pub.publish(n1)    # print fib sequence
-        nth = n1 + n2           # update value
-        n1 = n2
-        n2 = nth
+            rospy.loginfo(n1)
+            self.pub.publish(n1)
+            nth = n1 + n2
+            # update values
+            n1 = n2
+            n2 = nth
 
-    if __name__ == '__main__':
-        try:
-            rospy.init_node('talker', anonymous=True)
-            t = Talker()
-            rate = rospy.Rate(1)  # 1Hz
-            while not rospy.is_shutdown():
-                t.talk()
-                rate.sleep()
-        except rospy.ROSInterruptException:
-            pass
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('talker', anonymous=True)
+        t = Talker()
+        rate = rospy.Rate(.01)  # 1hz
+        while not rospy.is_shutdown():
+            t.talk()
+            rate.sleep()
+    except rospy.ROSInterruptException:
+        pass
