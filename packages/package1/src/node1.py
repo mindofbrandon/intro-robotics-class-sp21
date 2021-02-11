0dae1b208a40260d157ebc4b134777fe2852c4a8
brandon <brandon_zuniga@student.uml.edugit config --global user.email brandon_zuniga@student.uml.edugit config --global user.email brandon_zuniga@student.uml.edu>
2021-02-11 11:29:48 -0500

    trying new things

--- packages/package1/src/node1.py
+++ packages/package1/src/node1.py
@@ -8,8 +8,50 @@ n1, n2 = 0, 1               # initial terms for Fibonacci sequence
 
 
 class Talker:
+    def __init__(self): # run as soon as an object is created
+
+        # declares that node is publishing to topic (topic = chatter)
+        self.pub = rospy.Publisher('chatter', String, queue_size=10)
+
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('talker', anonymous=True) # tells the name of the node (node = talker)
+        # t = Talker()
+        rate = rospy.Rate(1)  # 1hz
+        while not rospy.is_shutdown():
+            # rospy.loginfo(n1)
+            # self.pub.publish(n1)
+            # print(n1)
+            nth = n1 + n2
+            # update values
+            n1 = n2
+            n2 = nth
+
+            rate.sleep()
+    except rospy.ROSInterruptException:
+        pass
+
+#######################################################################################################################
+# !/usr/bin/env python3
+# Adapted from http://wiki.ros.org/ROS/Tutorials/WritingPublisherSubscriber%28python%29
+
+import rospy
+from std_msgs.msg import String
+
+
+class Talker:
     def __init__(self):
         self.pub = rospy.Publisher('chatter', String, queue_size=10)
+
+    def talk(self):
+        if n <= 1:
+            return n
+        else:
+            return recur_fibo(n - 1) + recur_fibo(n - 2)
+
+        rospy.loginfo(hello_str)
+        self.pub.publish(hello_str)
 
 
 if __name__ == '__main__':
@@ -18,13 +60,7 @@ if __name__ == '__main__':
         t = Talker()
         rate = rospy.Rate(1)  # 1hz
         while not rospy.is_shutdown():
-            rospy.loginfo(n1)
-            print(n1)
-            nth = n1 + n2
-            # update values
-            n1 = n2
-            n2 = nth
-
+            t.talk()
             rate.sleep()
     except rospy.ROSInterruptException:
         pass
