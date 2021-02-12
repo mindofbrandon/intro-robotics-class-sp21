8a0ce2250866394e1f4dad944bacb6a2895ff853
brandon <brandon_zuniga@student.uml.edugit config --global user.email brandon_zuniga@student.uml.edugit config --global user.email brandon_zuniga@student.uml.edu>
2021-02-12 00:09:44 -0500

    added '/'

--- packages/package1/src/fibpubnode1.py
+++ packages/package1/src/fibpubnode1.py
@@ -14,7 +14,7 @@ def fib():
 
 
 def talker():
-    pub = rospy.Publisher("input", Float32, queue_size=10)  # node is publishing to input topic?
+    pub = rospy.Publisher("/input", Float32, queue_size=10)  # node is publishing to input topic?
     rospy.init_node('fibpubnode1', anonymous=True)  # tells rospy the name of node is "fibpubnode1"
     rate = rospy.Rate(1)  # 1Hz, loops once per second
     while not rospy.is_shutdown():
