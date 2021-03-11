6fb1210e8a11b7348de9662ae5161168e389a484
brandon <brandon_zuniga@student.uml.edugit config --global user.email brandon_zuniga@student.uml.edugit config --global user.email brandon_zuniga@student.uml.edu>
2021-03-11 17:56:01 -0500

    updating straight run code

--- packages/duckiemvmt/src/straightrun.py
+++ packages/duckiemvmt/src/straightrun.py
@@ -2,7 +2,7 @@
 
 import rospy
 from std_msgs.msg import Float32
-from duckietown_msgs.msg import Twist2DStamped  # contains Header header, float32 v, float32 omega
+from duckietown_msgs.msg import Twist2DStamped, FSMState, BoolStamped  # contains Header header, float32 v, float32 omega
 #
 # float32 v controls forward velocity
 # float32 omega controls angular velocity
@@ -10,9 +10,14 @@ from duckietown_msgs.msg import Twist2DStamped  # contains Header header, float3
 
 class StraightRun:
     def __init__(self):
-        self.pub = rospy.Publisher("/car_cmd_switch_node/cmd", Twist2DStamped, queue_size=10)  # publish to car_cmd to imitate lane following
+        self.pub = rospy.Publisher("car_cmd_switch_node/cmd", Twist2DStamped, queue_size=10)  # publish to car_cmd to imitate lane following
+        rospy.Subscriber("fsm_node/mode", FSMState, self.cb_state)  # subscribe
+        self.pub_state = FSMState()
         # add a subscriber to listen to fsm state
         # need a flag to figure out fsm state
+
+        # NORMAL_JOYSTICK_CONTROL
+        # LANE_FOLLOWING
 
     def movefwd(self, fwdvelocity):
         movement = Twist2DStamped()
@@ -24,17 +29,22 @@ class StraightRun:
         movement.v = 0
         self.pub.publish(movement)
 
+    def cb_state(self, x):
+        bruh = x
+        flag = FSMState()
+        rospy.loginfo("received %s, %s", flag, bruh)  # output the conversion
+
 
 if __name__ == '__main__':
     rospy.init_node('straightrun')
-    s = StraightRun()  # needs to be used for movefwd and stop functions
-    i = 0
-    rate = rospy.Rate(1)  # 1Hz, loops once per second
+    StraightRun()  # needs to be used for movefwd and stop functions
+    # i = 0
+    # rate = rospy.Rate(1)  # 1Hz, loops once per second
 
-    while i < 10:
-        s.movefwd(1)
-        i += 1
-    s.stop()
-
+    # while i < 10:
+    #     s.movefwd(1)
+    #     i += 1
+    # s.stop()
+    # cb_state(self)
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
