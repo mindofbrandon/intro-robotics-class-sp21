27832923e4b29bbc352000910723119e12ab6d65
brandon <brandon_zuniga@student.uml.edu>
2021-04-01 14:05:06 -0400

    updating odom_pose

--- packages/odometry/src/odom_calc.py
+++ packages/odometry/src/odom_calc.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# This is hw6 and done as a theoretical test
+# This is lab3 and done on the physical robot
 import rospy
 import math
 # from std_msgs.msg import Float32
@@ -29,7 +29,7 @@ class OdomPose:
         global theta_initial
         # rospy.loginfo("val i = %s", i)
 
-        while (i  < 1):
+        while (i < 1):
             xpos_initial = 0
             ypos_initial = 0
             theta_initial = 0
@@ -44,13 +44,13 @@ class OdomPose:
 
 
 
-        delta_s = (movement_left + movement_right) / 2  # arc length
-        delta_theta = (movement_right - movement_left) / (2 * .1)  # Assume that the baseline (distance) between wheels (front or back) 2L=0.1m (so L=0.05m) and wheel distances are given are in meters.
+        delta_s = (movement_left + movement_right) / 2.0  # arc length
+        delta_theta = (movement_right - movement_left) / (2.0 * .05)  # Assume that the baseline (distance) between wheels (front or back) 2L=0.1m (so L=0.05m) and wheel distances are given are in meters.
         # delta_theta = alpha
         # alpha is angle which equals delta_theta
 
-        val_cos = math.cos(theta_initial + (delta_theta / 2))
-        val_sin = math.sin(theta_initial + (delta_theta / 2))
+        val_cos = math.cos(theta_initial + (delta_theta / 2.0))
+        val_sin = math.sin(theta_initial + (delta_theta / 2.0))
 
         delta_x = delta_s * val_cos
         delta_y = delta_s * val_sin
@@ -74,7 +74,7 @@ class OdomPose:
         self.odom_positions.y = round(ypos_initial, 2)
         self.odom_positions.theta = theta_initial
 
-        rospy.loginfo("mvmt @ x: %f, mvmt @ y: %f, mvmt @ theta: %s", round(xpos_initial, 2), ypos_initial, theta_initial)
+        # rospy.loginfo("mvmt @ x: %f, mvmt @ y: %f, mvmt @ theta: %s", round(xpos_initial, 2), ypos_initial, theta_initial)
 
         self.odom.publish(self.odom_positions)
         # calculate left right and theta assuming robot starts @ x, y, z = 0
