8c238ab0e265dac678c723ad583cee0591a17bba
brandon <brandon_zuniga@student.uml.edu>
2021-04-15 22:20:20 -0400

    saving code, and trying new method

--- packages/ros_services/src/action_srv.py
+++ packages/ros_services/src/action_srv.py
@@ -1,40 +1,49 @@
 #!/usr/bin/env python3
 
-from Fibonacci.srv import uint64    # imports the service types
+from example_service.srv import Fibonacci, FibonacciResponse  # imports the service types
 import rospy
 
+# Brings in the SimpleActionClient
+import actionlib
+
+# inside Fibonacci.srv/Fibonacci:
+# uint64 order -- this is the request
+# ---
+# uint64[] sequence  -- this is the result
 
 ################## making a service available in your node
-def handle_add_two_ints(req):
-    print("Returning [%s + %s = %s]"%(req.a, req.b, (req.a + req.b)))   # is the function to handle the callback when service is called
-    return AddTwoIntsResponse(req.a + req.b)
 
-def add_two_ints_server():
+def handle_fib(req):
+    print(req)
+    rospy.loginfo("Returning: %s", str(req.order))  # is the function to handle the callback when service is called
+    return FibonacciResponse(req.order)
+
+
+def action_srv():
     rospy.init_node('action_srv')
-    # rospy.init_node('add_two_tints_server')
-    s = rospy.Service('add_two_ints', AddTwoInts, handle_add_two_ints)  # advertises the service and maps it to the callback
-    # print("Ready to add two ints.")
-    # rospy.spin()
-##################
-
-################## calling a service in your node
-
-def add_two_ints_client(x, y):
-    rospy.wait_for_service('add_two_ints')  # waits for service to become available
-    try:
-        add_two_ints = rospy.ServiceProxy('add_two_ints', AddTwoInts)   # setup the service
-        resp1 = add_two_ints(x, y)  # requests the service
-        return resp1.sum
-    except rospy.ServiceException as e:
-        print("Service call failed: %s"%e)
+    # rospy.init_node('add_two_ints_server')
+    s = rospy.Service('hw9_fib', Fibonacci, handle_fib)  # declares a new service named "hw9_fib_server" with type 'Fibonacci' -- similar to subscriber
+    rospy.loginfo("Ready to output fibonacci numbers")
+    rospy.spin()
 
 
 ##################
 
+################## calling a service in your node/ writing the client node
+
+# def add_two_ints_client(x):
+#    rospy.wait_for_service('calc_fibonacci')  # waits for service to become available
+#    try:
+#        action_srv = rospy.ServiceProxy('action_srv', Fibonacci)  # setup the service
+#        resp1 = action_srv(x)  # requests the service
+#        return resp1.sum
+#    except rospy.ServiceException as e:
+#        print("Service call failed: %s" % e)
 
 
+##################
 
 if __name__ == "__main__":
-    add_two_ints_server()
-    add_two_ints_client(1,2)
+    action_srv()
+    #   add_two_ints_client(1, 2)
     rospy.spin()
