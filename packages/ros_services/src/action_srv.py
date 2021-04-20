3dc8bd493bb5ab2bdcb178ddc2eca3613baaee26
brandon <brandon_zuniga@student.uml.edu>
2021-04-20 18:23:51 -0400

    cleaning index

--- packages/ros_services/src/action_srv.py
+++ packages/ros_services/src/action_srv.py
@@ -14,7 +14,6 @@ import actionlib
 ################## making a service available in your node
 
 def handle_fib(req):
-    print(req)
     rospy.loginfo("Returning: %s", str(req.order))  # is the function to handle the callback when service is called
     return FibonacciResponse(req.order)
 
@@ -22,7 +21,7 @@ def handle_fib(req):
 def action_srv():
     rospy.init_node('action_srv')
     # rospy.init_node('add_two_ints_server')
-    s = rospy.Service('hw9_fib', Fibonacci, handle_fib)  # declares a new service named "hw9_fib_server" with type 'Fibonacci' -- similar to subscriber
+    s = rospy.Service('action_srv', Fibonacci, handle_fib)  # declares a new service named "hw9_fib_server" with type 'Fibonacci' -- similar to subscriber
     rospy.loginfo("Ready to output fibonacci numbers")
     rospy.spin()
 
@@ -31,19 +30,20 @@ def action_srv():
 
 ################## calling a service in your node/ writing the client node
 
-# def add_two_ints_client(x):
-#    rospy.wait_for_service('calc_fibonacci')  # waits for service to become available
-#    try:
-#        action_srv = rospy.ServiceProxy('action_srv', Fibonacci)  # setup the service
-#        resp1 = action_srv(x)  # requests the service
-#        return resp1.sum
-#    except rospy.ServiceException as e:
-#        print("Service call failed: %s" % e)
+# running /rosservice call /calc_fibonacci 3 is not working?
 
+def add_two_ints_client(x):
+    rospy.wait_for_service('action_srv')  # waits for service to become available
+    try:
+        action_srv = rospy.ServiceProxy('calc_fibonacci', Fibonacci)  # setup the service -- similar to subscribing to calc_fibonacci?
+        resp1 = action_srv(x)  # requests the service
+        return resp1.sequence
+    except rospy.ServiceException as e:
+        print("Service call failed: %s" % e)
 
 ##################
 
 if __name__ == "__main__":
     action_srv()
-    #   add_two_ints_client(1, 2)
+    add_two_ints_client(3)
     rospy.spin()
